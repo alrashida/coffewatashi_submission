0cd6937bc83b730a481460520b1f7f7932540173
Alrashida <bintangalarashid@gmail.com>
2022-03-09 11:50:47 +0700

    halo w updateye

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:coffewatashi_submission/view/main.dart';
+import 'package:coffewatashi_submission/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
